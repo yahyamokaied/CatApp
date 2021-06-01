b070b61fa73bef80eb865473323c8878282d7995
Yahya <yahya@MacBook-Pro.local>
2021-06-01 23:27:25 +0200

    v-if v-else i CatNames.js

--- src/CatNames.js
+++ src/CatNames.js
@@ -7,9 +7,16 @@ const CatNames = ({ search }) => {
         fontSize: 80
     }
 
+    const cats = () => {
+        if (search) return <p>cat</p>
+        else return <p>no cat</p>
+    }
+
     return (
         <div className="App">
             <h1>My favorite Cats :{search ? <span style={MyCatColor}>{search}</span> : <span> No Cat Selected</span>}</h1>
+            {search && <h1>Cat: {search} </h1>}
+            {cats}
         </div>
     );
 }
